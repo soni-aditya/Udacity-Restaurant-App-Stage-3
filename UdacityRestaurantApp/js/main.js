944b21ab1925a2aaaa18bb0f3febeeda29c294f1
Aditya Soni <adityasoni5859@gmail.com>
2018-11-18 18:43:21 +0530

    Added functionality for showing toast.

--- UdacityRestaurantApp/js/main.js
+++ UdacityRestaurantApp/js/main.js
@@ -219,3 +219,9 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 };
+
+function showSnackBar() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
