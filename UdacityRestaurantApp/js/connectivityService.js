90e890d9fd94d9bf2417d83db561aa63900a152a
Aditya Soni <adityasoni5859@gmail.com>
2018-11-24 22:58:31 +0530

    feature : added toast alert for online/offline status in restaurant_info.html

--- UdacityRestaurantApp/js/connectivityService.js
+++ UdacityRestaurantApp/js/connectivityService.js
@@ -1,17 +1,22 @@
-class CheckConnectivity {
-  checkStatus = () => {
-    if (navigator.onLine) {
-      this.showSnackBar("Application is online.");
-    } else {
-      this.showSnackBar("No network connection available.");
-    }
-  };
-  showSnackBar = messageText => {
-    var snackBar = document.getElementById("snackbar");
-    snackBar.className = "show";
-    snackBar.innerHTML = messageText;
-    setTimeout(() => {
-      snackBar.className = snackBar.className.replace("show", "");
-    }, 3000);
-  };
+showSnackBar = messageText => {
+  var snackBar = document.getElementById("snackbar");
+  snackBar.className = "show";
+  snackBar.innerHTML = messageText;
+  setTimeout(() => {
+    snackBar.className = snackBar.className.replace("show", "");
+  }, 3000);
+};
+
+
+checkConnectivityStatus = () =>{
+  if (navigator.onLine) {
+    showSnackBar("You are currently online!");
+  } else {
+    showSnackBar(
+      "You are currently offline. Any requests made will be queued and synced as soon as you are connected again."
+    );
+  }
 }
+window.addEventListener("online", checkConnectivityStatus);
+window.addEventListener("offline", checkConnectivityStatus);
+checkConnectivityStatus();
